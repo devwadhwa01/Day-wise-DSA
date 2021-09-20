0c4885841ab612e5e3340e22c5d00050cd6c26aa
Dev Wadhwa <73272997+devwadhwa01@users.noreply.github.com>
2021-09-20 17:40:19 +0530

    Create Rotate Array.cpp

---
+++ Solutions/Day 1/Rotate Array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int> res;
+        int n=nums.size();
+        if(k>n) k%=n;
+        for(int i=n-k;i<n;i++)
+        {
+            res.push_back(nums[i]);
+        }
+        for(int i=0;i<n-k;i++)
+        {
+            res.push_back(nums[i]);
+        }
+        nums=res;
+    }
+};
+
+
+//Better Approach (O(1) space complexity)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k%=nums.size();
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin()+k,nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+    }
+};
